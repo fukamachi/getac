ba18885e998d7aa885978215d1919abec19fefa4
Eitaro Fukamachi <e.arrows@gmail.com>
2019-12-17 22:33:17 +0900

    Show a specific error message when a directory path is passed.

--- src/utils.lisp
+++ src/utils.lisp
@@ -5,6 +5,9 @@
 
 (defun normalize-pathname (filename)
   (let ((file (probe-file filename)))
-    (unless file
-      (error "File not exists: ~A" filename))
-    file))
+    (cond
+      ((null file)
+       (error "File not exists: ~A" filename))
+      ((uiop:directory-pathname-p file)
+       (error "Is a directory: ~A" filename))
+      (t file))))
